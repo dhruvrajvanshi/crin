f0cfe9b4b2efde08cea4d581d5dcb2eaff07ab83
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2021-08-01 21:02:10 +0530

    Implement FPut[Chunk]

--- src/crin/utils.c
+++ src/crin/utils.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stddef.h>
 
 void fprintf_u32(FILE* file, const char* format_string, uint32_t value) {
     fprintf(file, format_string, value);
@@ -10,5 +11,9 @@ void fprintf_usize(FILE* file, const char* format_string, size_t value) {
 }
 
 void fprintf_u8(FILE* file, const char* format_string, uint8_t value) {
+    fprintf(file, format_string, value);
+}
+
+void fprintf_f64(FILE* file, const char* format_string, double value) {
     fprintf(file, format_string, value);
 }
